8f87024208a45753c7204e46804065c0596364fe
lfchen <levon.chen@foxmail.com>
2017-11-16 20:09:34 +0800

    update index

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var net = require('net');
 var util = require('util');
+var path = require('path');
+var fs = require('fs');
 //var redis	= require('redis');
 
 //var client = redis.createClient('6379','redis');
@@ -13,6 +15,12 @@ function a(){
 }
 
 var visitCount = 0;
+var rootFolder = "/lf_data/";
+if(path.sep == "\\"){
+	rootFolder = "D:\\"
+}
+
+var fileLogPath = path.join(rootFolder,"lf_log.txt");
 
 var server = new http.Server();
 server.on('request',(req,res) =>{
@@ -34,6 +42,10 @@ server.on('request',(req,res) =>{
 	res.writeHead(200,{'Content-Type':'text/html'});
 	res.write('hello, This page has been viewed ' + visitCount + ' times ! <br/>');		
 	res.end("End")
+	
+	fs.appendFile(fileLogPath,"\r\n A:" + visitCount,function(err){
+		console.error(err);
+	})
 	
 });
 
